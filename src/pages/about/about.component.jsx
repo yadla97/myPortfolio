0bf54f0df358fb4d76367d6e891593a5f7d20bbc
Anjali <yadla1997@gmail.com>
2021-03-04 11:22:37 -0600

    changes in about page

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -27,7 +27,7 @@ const About = () => {
                             <Row className=" align-items-start p-2 my-details rounded">
                                 Hi there! I am <strong>&nbsp;Anjali Yadla</strong>
                                 {/* <br />I am a Master's student at University of Illinois at Chicago. */}
-                                <br />I am a graduate student in the School of Computer Science at University of Illinois, Chicago. I am pursuing a Master of Computer Science degree.
+                                <br />I am a graduate student in the School of University of Illinois, Chicago. I am pursuing a Master of Computer Science degree.
                                 <br />I am broadly interested in Software Development with strong fundamentals in algorithms, dynamic programming, object oriented programming and relational databases.
                                 <br />I worked as a Programmer Analyst with React.js, Express.js, node.js, JavaScript, MongoDB as my tech stack.
                                 <br />
