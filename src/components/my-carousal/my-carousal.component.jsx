3a8fb0d985a7828b00a6a1cd04d9aca943bcd10c
Anjali <yadla1997@gmail.com>
2021-03-04 09:58:42 -0600

    background images chnaged

--- src/components/my-carousal/my-carousal.component.jsx
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Carousel from "react-bootstrap/Carousel";
-import Slide1 from '../../assets/img/carousal/slide1.webp';
-import Slide2 from '../../assets/img/carousal/slide2.webp';
-import Slide3 from '../../assets/img/carousal/slide3.webp';
+import Slide1 from '../../assets/img/carousal/s5.jpg';
+import Slide2 from '../../assets/img/carousal/s2.jpg';
+import Slide3 from '../../assets/img/carousal/s3.jpg';
 import ScrollDown from '../scroll-down/scroll-down.component';
 
 
--- src/pages/experience/experience.component.jsx
+++ src/pages/experience/experience.component.jsx
@@ -25,21 +25,19 @@ return (
                   <Card.Text className="text-center style">
                     <strong className="body-title-style ">Programmer Analyst</strong>
                     <br />
-                    <strong>Technology:</strong> React JS, JavaScript , C#, MS-SQL, Python
+                    <strong>Technology:</strong> node.js, JavaScript , HTML, MongoDB, Express.js
                     <br />
                     <strong>Duration:</strong> May 2018 - May 2019
                     <br/>
                     <strong> Description </strong>
                     <ul className="text-left">
-                      <li><strong>Developed &amp; enhanced</strong> multiple features with customizability option across web application.</li>
-                      <li><strong>Developed</strong> automation system to create SQL bulk query scripts that 
-                        increased efficiency by 80% and decreased working hours from 4 hours to 30 mins per task.
-                      </li>
-                      <li><strong>Provided</strong> application maintenance while working as `Production Support`. 
-                      </li>
-                      <li><strong>Performed</strong> CRUD operations on multiple databases to load/ remove data according 
-                      to the business requirements.</li>
-                      {/* <li><strong>Co-created</strong> React Document used as a guide for new developers.</li> */}
+                    <li>A web application developed from scratch using HTML 5, CSS 3, Bootstrap, node.js, express.js and MongoDB as
+                    database on AWS</li>
+                    <li>Developed outstanding responsive layout and used node.js libraries like Mongoose, crypto</li>
+                    <li>This site contains features like sign up, sign in and a list of books that user can rate and add his/her favorite books
+                    to cart</li>
+                    <li>Tech stack: AWS, HTML, CSS, JavaScript, node.js, MongoDB</li>
+                      
 
                     </ul>
                     
